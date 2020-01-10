52169241abff965c95dc0f6b3d858b70dc732607
melai-melai <melai1777@yandex.ru>
2020-01-10 23:00:41 +0300

    CRUD: resource

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,85 +2,110 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreArticle;
-use App\Article;
 
 class ArticleController extends Controller
 {
-  public function index(Request $request)
-  {
-    $status = $request->session()->get('status');
-    $articles = Article::paginate(2);
-    return view('article.index', compact('articles', 'status'));
-  }
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $status = $request->session()->get('status');
+        $articles = Article::paginate(2);
+        return view('article.index', compact('articles', 'status'));
+    }
 
-  public function show($id)
-  {
-    $article = Article::findOrFail($id);
-    return view('article.show', compact('article'));
-  }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
 
-  // Вывод формы
-  public function create()
-  {
-    // Передаём в шаблон вновь созданный объект. Он нужен для вывода формы через Form::model
-    $article = new Article();
-    return view('article.create', compact('article'));
-  }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param App\Http\Requests\StoreArticle  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreArticle $request)
+    {
+        $validatedData = $request->validated();
+        $article = new Article();
+        $article->fill($validatedData);
+        $article->save();
+        $request->session()->flash('status', 'Store article was successful!');
+        return redirect()
+          ->route('articles.index');
+    }
 
-  public function store(StoreArticle $request)
-  {
-    $validatedData = $request->validated();
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
+    {
+        return view('article.show', compact('article'));
+    }
 
-    $article = new Article();
-    // Заполнение статьи данными из формы
-    $article->fill($validatedData);
-    // При ошибках сохранения возникнет исключение
-    $article->save();
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article $article)
+    {
+        return view('article.edit', compact('article'));
+    }
 
-    $request->session()->flash('status', 'Store article was successful!');
-
-    // Редирект на указанный маршрут с добавлением флеш-сообщения
-    return redirect()
-      ->route('articles.index');
-  }
-
-  public function edit($id)
-  {
-    $article = Article::findOrFail($id);
-    return view('article.edit', compact('article'));
-  }
-
-  public function update(Request $request, $id)
-  {
-      $article = Article::findOrFail($id);
-      $this->validate($request, [
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article $article)
+    {
+        $this->validate($request, [
           // У обновления немного изменённая валидация. В проверку уникальности добавляется название поля и id текущего объекта
           // Если этого не сделать, Laravel будет ругаться на то что имя уже существует
           'name' => 'required|unique:articles,name,' . $article->id,
           'body' => 'required|min:5',
-      ]);
-
-      $article->fill($request->all());
-      $article->save();
-
-      $request->session()->flash('status', 'Edit article was successful!');
-
-      return redirect()
+        ]);
+        $article->fill($request->all());
+        $article->save();
+        $request->session()->flash('status', 'Edit article was successful!');
+        return redirect()
           ->route('articles.index');
-  }
+    }
 
-  public function destroy(Request $request, $id)
-  {
-      // DELETE — идемпотентный метод, поэтому результат операции всегда один и тот же
-      $article = Article::find($id);
-      if ($article) {
-        $article->delete();
-      }
-
-      $request->session()->flash('status', 'Delete article was successful!');
-
-      return redirect()->route('articles.index');
-  }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, Article $article)
+    {
+        if ($article) {
+            $article->delete();
+        }
+        $request->session()->flash('status', 'Delete article was successful!');
+        return redirect()->route('articles.index');
+    }
 }
